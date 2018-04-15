6270405e96ff14f4f19f8a0106d4bebc0948be6a
redgemini <wendykiddy@Wendys-MBP.lan1>
2018-04-14 23:16:23 -0500

    fileSetUp

---
+++ bamazonCustomer/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT(10) NOT NULL,
+  primary key(item_id)
+);
+
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
